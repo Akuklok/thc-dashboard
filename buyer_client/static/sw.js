a039496cdab6f9386b599fc06549888366773129
Akuklok <akuklok@toptenliquors.com>
2026-06-24 15:34:33 -0500

    deploy app: index.html, sw.js

--- buyer_client/static/sw.js
+++ buyer_client/static/sw.js
@@ -1,6 +1,6 @@
 // Service worker: network-first for the app page (so updates show immediately),
 // cache-first for icons, never cache /api/ data. Bump CACHE to force a refresh.
-const CACHE = 'ttb-v35';
+const CACHE = 'ttb-v36';
 const SHELL = ['/icon.svg', '/manifest.webmanifest'];
 
 self.addEventListener('install', e => {
